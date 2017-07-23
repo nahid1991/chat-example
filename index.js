68ac5f448441d2ee39207942a19f1df406836f95
Nahid <nahidshaiket10300@gmail.com>
2017-07-23 14:30:14 +0600

    to study at home

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4200;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
