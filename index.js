ad16c2092a05cb70a31bc9a102ca05b3c0b5a2f0
Nahid <nahidshaiket10300@gmail.com>
2017-07-25 11:33:07 +0600

    found socket id

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(socket.id);
+    socket.broadcast.to(socket.id).emit('chat message', 'for your eyes only');
   });
 });
 
