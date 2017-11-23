facebae71b9b3c61358b477e88031efca54001e8
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-11-23 11:23:13 +0600

    friend socket work

--- api/controllers/friendsController.js
+++ api/controllers/friendsController.js
@@ -1,24 +1,24 @@
 'use strict';
 
 let mongoose = require('mongoose'),
-	Users = mongoose.model('Users'),
-	Friends = mongoose.model('Friends'),
-	Token = mongoose.model('Token');
+    Users = mongoose.model('Users'),
+    Friends = mongoose.model('Friends'),
+    Token = mongoose.model('Token');
 
 exports.getPeople = function (req, res) {
     let extract = req.header('Authorization').split(" ");
     let token = extract[1];
 
-    Token.findOne({'token': token}).then(async function(token){
-        if(token == null) {
+    Token.findOne({'token': token}).then(async function (token) {
+        if (token == null) {
             console.log(err);
-			res.json({
-				'error': 'Token mismatch'
-			});
+            res.json({
+                'error': 'Token mismatch'
+            });
         } else {
-        	getPeopleHelper(req.params.letters, req.query.page, token.user, res);
+            getPeopleHelper(req.params.letters, req.query.page, token.user, res);
         }
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
         throw 500;
     });
@@ -26,94 +26,102 @@ exports.getPeople = function (req, res) {
 
 
 function getPeopleHelper(letters, page, user, res) {
-	Users.paginate({$and: [{name: {$regex: '.*' + letters + '.*', $options: "i"}}, {_id: {$ne: user}}]}, {
-		page: page,
-		limit: 10
-	}).then(async function(people){
-		let usersList = JSON.parse(JSON.stringify(people));
-		for(let i=0; i<usersList.docs.length; i++){
-			let verify = await Users.isFriend(user, usersList.docs[i]._id);
-			usersList.docs[i].chat_room = verify.chat_room;
-			usersList.docs[i].accepted = verify.accepted;
-			usersList.docs[i].friend = verify.friend;
-			usersList.docs[i].initiator = verify.initiator;
-		}
-		res.json(usersList);
-	}).catch(function(err){
-		console.log(err);
-		throw 500;
-	});
+    Users.paginate({$and: [{name: {$regex: '.*' + letters + '.*', $options: "i"}}, {_id: {$ne: user}}]}, {
+        page: page,
+        limit: 10
+    }).then(async function (people) {
+        let usersList = JSON.parse(JSON.stringify(people));
+        for (let i = 0; i < usersList.docs.length; i++) {
+            let verify = await Users.isFriend(user, usersList.docs[i]._id);
+            usersList.docs[i].chat_room = verify.chat_room;
+            usersList.docs[i].accepted = verify.accepted;
+            usersList.docs[i].friend = verify.friend;
+            usersList.docs[i].initiator = verify.initiator;
+        }
+        res.json(usersList);
+    }).catch(function (err) {
+        console.log(err);
+        throw 500;
+    });
 }
 
 
 exports.getFriends = function (req, res) {
-	let extract = req.header('Authorization').split(" ");
-	let token = extract[1];
-	
-	Token.findOne({'token': token}).then(function(token){
-		if(token === null) {
-			res.json({
-				'error': 'Token mismatch'
-			});
-		} else {
-			Friends.paginate({$or:[{user: token.user, accepted: true}, {user: token.user, accepted: false, initiator: {$ne: token.user}}]},
-				{page: req.query.page, limit: 10, populate: 'friend', lean: true, sort:{last_talked: -1}})
-			.then(function(friends){
-				let friendsData = {
-					docs : [],
-				};
+    let extract = req.header('Authorization').split(" ");
+    let token = extract[1];
 
-                for(let i=0; i<friends.docs.length; i++){
-                    friendsData.docs.push(friends.docs[i].friend);
-                }
-
-                for(let i=0; i<friendsData.docs.length; i++){
-                    friendsData.docs[i].friend = true;
-                    friendsData.docs[i].accepted = friends.docs[i].accepted;
-                    friendsData.docs[i].chat_room = friends.docs[i].chat_room;
-                }
-
-                friendsData.total = friends.total;
-                friendsData.limit = friends.limit;
-                friendsData.page = friends.page;
-                friendsData.pages = friends.pages;
-				res.json(friendsData);
-			}).catch(function(err){
-				console.log(err);
-				throw 500;
-			});
-		}
-	}).catch(function(err){
-		console.log(err);
-		throw 500;
-	});
-};
-
-
-exports.removeFriends = function(req, res) {
-	let extract = req.header('Authorization').split(" ");
-	let token = extract[1];
-
-    Token.findOne({'token': token}).then(function(token){
-        if(token === null) {
+    Token.findOne({'token': token}).then(function (token) {
+        if (token === null) {
             res.json({
                 'error': 'Token mismatch'
             });
         } else {
-        	Friends.remove({$or: [{user: token.user, friend: req.params.friendId},
-				{user: req.params.friendId, friend: token.user}]}).then(function(response) {
-					console.log(response);
-					res.json({
-						status: 'success'
-					});
-			}, function(err){
-					console.log(err);
-					res.json({
-						status: 'failed'
-					});
-			});
+            Friends.paginate({
+                    $or: [{user: token.user, accepted: true}, {
+                        user: token.user,
+                        accepted: false,
+                        initiator: {$ne: token.user}
+                    }]
+                },
+                {page: req.query.page, limit: 10, populate: 'friend', lean: true, sort: {last_talked: -1}})
+                .then(function (friends) {
+                    let friendsData = {
+                        docs: [],
+                    };
+
+                    for (let i = 0; i < friends.docs.length; i++) {
+                        friendsData.docs.push(friends.docs[i].friend);
+                    }
+
+                    for (let i = 0; i < friendsData.docs.length; i++) {
+                        friendsData.docs[i].friend = true;
+                        friendsData.docs[i].accepted = friends.docs[i].accepted;
+                        friendsData.docs[i].chat_room = friends.docs[i].chat_room;
+                    }
+
+                    friendsData.total = friends.total;
+                    friendsData.limit = friends.limit;
+                    friendsData.page = friends.page;
+                    friendsData.pages = friends.pages;
+                    res.json(friendsData);
+                }).catch(function (err) {
+                console.log(err);
+                throw 500;
+            });
         }
-    }).catch(function(err){
+    }).catch(function (err) {
+        console.log(err);
+        throw 500;
+    });
+};
+
+
+exports.removeFriends = function (req, res) {
+    let extract = req.header('Authorization').split(" ");
+    let token = extract[1];
+
+    Token.findOne({'token': token}).then(function (token) {
+        if (token === null) {
+            res.json({
+                'error': 'Token mismatch'
+            });
+        } else {
+            Friends.remove({
+                $or: [{user: token.user, friend: req.params.friendId},
+                    {user: req.params.friendId, friend: token.user}]
+            }).then(function (response) {
+                console.log(response);
+                res.json({
+                    status: 'success'
+                });
+            }, function (err) {
+                console.log(err);
+                res.json({
+                    status: 'failed'
+                });
+            });
+        }
+    }).catch(function (err) {
         console.log(err);
         res.json({
             'error': 'Token mismatch'
@@ -122,14 +130,14 @@ exports.removeFriends = function(req, res) {
 };
 
 exports.postFriends = function (req, res) {
-	let extract = req.header('Authorization').split(" ");
-	let token = extract[1];
-	Token.findOne({'token': token}).then(function(token){
-		if(token === null) {
-			res.json({
-				'error': 'Token mismatch'
-			});
-		} else {
+    let extract = req.header('Authorization').split(" ");
+    let token = extract[1];
+    Token.findOne({'token': token}).then(function (token) {
+        if (token === null) {
+            res.json({
+                'error': 'Token mismatch'
+            });
+        } else {
             let newFriends = new Friends(
                 {
                     user: token.user,
@@ -156,10 +164,10 @@ exports.postFriends = function (req, res) {
 
             res.json(newFriends);
         }
-	}).catch(function(err){
-		console.log(err);
-		res.json({
-			'error': 'Token mismatch'
-		});
-	});
+    }).catch(function (err) {
+        console.log(err);
+        res.json({
+            'error': 'Token mismatch'
+        });
+    });
 };
--- api/sockets/chatSocket.js
+++ api/sockets/chatSocket.js
@@ -4,53 +4,61 @@ let mongoose = require('mongoose'),
     Users = mongoose.model('Users'),
     Friends = mongoose.model('Friends');
 
-module.exports = function (io){
+module.exports = function (io) {
     // this function expects a socket_io connection as argument
     // now we can do whatever we want:
-    io.on('connection', function(socket){
+    io.on('connection', function (socket) {
         console.log('a user is connected', socket.id);
 
         socket.on('join room', function (room) {
-            try{
+            try {
                 socket.join(room);
             } catch (err) {
                 console.log(new Error(err))
             }
         });
 
-        socket.on('message', function(msg) {
+        socket.on('message', function (msg) {
             io.emit(msg.chat_room_user, msg);
             io.emit(msg.chat_room_friend, msg);
         });
 
-        socket.on('removeFriend', function(data, fn){
-            Friends.remove({$or: [{user: data.sender, friend: data.receiver},
-                {user: data.receiver, friend: data.sender}]}).then(function(response) {
-                    fn({success: true});
-            }, function(err){
+        socket.on('removeFriend', function (data, fn) {
+            Friends.remove({
+                $or: [{user: data.sender, friend: data.receiver},
+                    {user: data.receiver, friend: data.sender}]
+            }).then(function (response) {
+                fn({success: true});
+            }, function (err) {
                 console.log(err);
                 fn({success: false});
             });
         });
-	
-		socket.on('acceptFriend', function(data, fn){
-			Friends.update({user: data.sender, friend: data.receiver}, {$set: {accepted: true}}).then(function(response) {
-			    console.log(response);
-			    Friends.update({user: data.receiver, friend: data.sender}, {$set: {accepted: true}}).then(function(res){
-					console.log(res);
-			        fn({success: true});
-                }, function(err){
-			        console.log(err);
-			        fn({success: false});
-                });
-				
-			}, function(err){
-				console.log(err);
-				fn({success: false});
-			});
-		});
 
-        socket.on('addFriend', function(data, fn){
+        socket.on('acceptFriend', function (data, fn) {
+            Friends.update({
+                user: data.sender,
+                friend: data.receiver
+            }, {$set: {accepted: true}}).then(function (response) {
+                console.log(response);
+                Friends.update({
+                    user: data.receiver,
+                    friend: data.sender
+                }, {$set: {accepted: true}}).then(function (res) {
+                    console.log(res);
+                    fn({success: true});
+                }, function (err) {
+                    console.log(err);
+                    fn({success: false});
+                });
+
+            }, function (err) {
+                console.log(err);
+                fn({success: false});
+            });
+        });
+
+        socket.on('addFriend', function (data, fn) {
             let newFriends = new Friends(
                 {
                     user: data.sender,
@@ -71,16 +79,20 @@ module.exports = function (io){
                     initiator: data.sender,
                     chat_room: data.receiver + '-' + data.sender,
                     accepted: false,
-					last_talked: Date.now()
+                    last_talked: Date.now()
                 }
             );
 
             newFriendsTwo.save();
 
             fn({success: true});
+
+            Users.findOne({_id: data.sender}).then(function(user){
+                io.emit(data.receiver+'-receivedRequest', user);
+            });
         });
 
-        socket.on('disconnect', function(){
+        socket.on('disconnect', function () {
             console.log('a user disconnected', socket.id);
         });
     });
